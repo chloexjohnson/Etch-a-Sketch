1187b2511f68d03422c670f933adc47acfd13a79
chloexjohnson <cjohnson@hyperiontg.com>
2024-01-22 12:38:06 -0500

    Update to Etch-a-Sketch

--- main.js
+++ main.js
@@ -1,16 +1,80 @@
+// selectining container div as our query
 const containerDiv = document.querySelector("#container");
 
-function makeRows(rows, columns) {
-    containerDiv.computedStyleMap.setProperty("--grid-rows", rows);
-    containerDiv.computedStyleMap.setProperty("--grid-columns", columns);
-    console.log(containerDiv);
-    console.log(rows);
-    console.log(columns);
-    for ( i = 0; i < (rows * columns); i++) {
-        let square = document.createElement("div");
-        square.innerText = (i + 1); // labels the squares
-        containerDiv.appendChild(square).className = "grid-item";
+function makeGrid(rows, columns) {
+
+    // check for existing button to remove if played already
+    // check for exisiting button to remove if played already
+    // while the button is not null then it is going to remove the button
+    while (document.querySelector("button") !== null) {
+        document.querySelector("button").remove();
     }
+    
+    // create the grid 
+    containerDiv.style.setProperty("--grid-rows", rows);
+    containerDiv.style.setProperty("--grid-columns", columns);
+    containerDiv.style.width = "960px"; // sets the width of the div to 960px wide when you invoke the button
+    containerDiv.style.overflow = "hidden"; // helps with bleeding
+    for (i = 0; i < (rows * columns); i++) {
+        let square = document.createElement("div");
+        square.style.minHeight = "0";
+        square.style.minWidth = "0";
+        square.style.overflow = "hidden";
+        containerDiv.appendChild(square).className = "grid-item"; // appending square to the containerDiv
+        
+        // add event listen to check for background color presence
+        square.addEventListener("mouseover", () => {
+            
+            // run check to see if background color is present, if NOT apply random color; apply 10% opacity interval
+            if (square.style.backgroundColor == "") {
+                let color = getRandomColor();
+                square.style.backgroundColor = color;
+                square.style.opacity = ".10";
+                return square.style.backgroundColor;
+            }
+            // apply additional opacity at 10% intervals, hard stop at 1.0 (100%) IF background color is present
+            if ((square.style.backgroundColor !== "") && (square.style.opacity <= "0.90")) {
+                square.style.opacity = parseFloat(square.style.opacity) + .10;
+                return square.style.backgroundColor;
+            }    
+        }) 
+    }
+    
+    createButton();
 }
 
-makeRows(16, 16);
+function createButton() {
+    const buttonDiv = document.querySelector("#buttonDiv"); // calls buttonDiv in the html file 
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Reset Grid!";
+    resetButton.style.margin = "20px";
+    buttonDiv.appendChild(resetButton); // appending the resetButton to buttonDiv
+
+    // add event listen to button and prompt user/reset grid/throw error > 100
+    resetButton.addEventListener('click', () => {
+        document.querySelectorAll(".grid-item").forEach(e => e.remove());
+        let userGridInput = prompt("Please enter the number of grid squares per side (Max: 100): ");
+        if (userGridInput > 100) {
+            alert("ERROR!  You specified a grid size larger than the max of 100.");
+            return;
+        }
+        rows = userGridInput;
+        columns = userGridInput;
+        makeGrid(rows, columns);
+        })
+}
+
+function getRandomColor() {
+    let o = Math.round;
+    let r = Math.random;
+    let s = 255;
+    return "rgb(" + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ')';
+}
+
+// make initial call on page load as per project requirements
+makeGrid(16, 16);
+
+
+
+
+
